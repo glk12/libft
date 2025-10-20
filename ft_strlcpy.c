e66a25dadb66f9c5a971c8e33a5a2c2154f6e993
glk12 <glopesaraujo6@gmail.com>
2025-10-19 21:20:35 -0300

    42

---
+++ ft_strlcpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glopes-a <glopes-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 15:09:02 by glopes-a          #+#    #+#             */
+/*   Updated: 2025/10/18 15:25:03 by glopes-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	len;
+		
+	len = ft_strlen(src);
+	if (size == 0)
+		return (len);
+	while (*src && size > 1)
+	{
+		*dst++ = *src++;
+		size--;
+	}
+	*dst = '\0';
+	return (len);
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	char *s1 = "778899";
+	char s2[5] = "";
+	int	len = 5;
+	ft_strlcpy(s2,s1,len);
+	printf("%s", s2);
+}
+*/
